0c4bcf9688e2c60d267d66a1b54f9659a0e2846f
socamina <55089549+socamina@users.noreply.github.com>
2021-05-27 19:53:22 +0200

    g bsoin d'éd

--- js/DiscordBotServos.js
+++ js/DiscordBotServos.js
@@ -13,20 +13,15 @@ class DiscordBotServos {
     this.users = [];
     this.usersIndex = {};
     this.board;
-
     this.enabled = false;
-    // this.servo1;
-    // this.servo2;
-    // this.servo3;
-    // this.servo4;
     this.button;
+    this.replies = ["Je suis indisponible pour le moment, je répond après.","J'arrive","Attendez, je vous répond plus tard","J'ai pas le temps de vous lire maintenant","Je suis occupée"];
     this.initArduino();
   }
 
   initArduino() {
     this.board = new Board({ repl: true });
     this.board.on("ready", this.onBoardReady.bind(this));
-
     console.log("BOARD STARTED");
   }
 
@@ -40,6 +35,7 @@ class DiscordBotServos {
     const channel = this.client.channels.cache.get("836975500525174804");
 
     this.button = new Button(14);
+
     this.servos = new Servos([
       { pin: 2, invert: true },
       { pin: 3 },
@@ -59,7 +55,8 @@ class DiscordBotServos {
 
     this.button.on("down", () => {
       if (this.enabled) {
-        channel.send("Je vous réponds plus tard.");
+        var randomReply = this.replies[Math.floor(Math.random()*this.replies.length)];
+        channel.send(randomReply);
       } else {
         console.log("button enabled");
         this.enabled = true;
@@ -75,8 +72,8 @@ class DiscordBotServos {
       this.resyncServo(index);
     });
 
-    await delay(6000);
-    console.log("j'attend");
+    await delay(4000);
+    console.log("waiting over you can gooo");
   }
 
   onReady() {
@@ -90,7 +87,8 @@ class DiscordBotServos {
     this.users.pop();
     console.log(this.users);
   }
-  async arpege(accord, { duration = 2000 } = {}) {
+
+  async arpeggio(accord, { duration = 800 } = {}) {
     for (let servoIndex of accord) {
       this.swipeToOppositeSide(servoIndex);
       await delay(duration);
@@ -125,41 +123,40 @@ class DiscordBotServos {
 
     this.win.webContents.send("messageDiscord", messageInfo);
 
-    // ARPEGE PARTS
+    // ARPEGGIO BIT
 
     switch (messageInfo.author) {
       case this.users[0]:
-        await this.arpege([2, 4, 6, 9]);
+        await this.arpeggio([0, 2, 4, 7]);
         break;
       case this.users[1]:
-        await this.arpege([2, 4, 6, 9]);
+        await this.arpeggio([1, 3, 5, 8]);
         break;
       case this.users[2]:
-        await this.arpege([2, 4, 6, 9]);
+        await this.arpeggio([2, 4, 6, 9]);
         break;
       case this.users[3]:
-        await this.arpege([2, 4, 6, 9]);
+        await this.arpeggio([3, 5, 7, 10]);
         break;
       case this.users[4]:
-        await this.arpege([2, 4, 6, 9]);
+        await this.arpeggio([4, 6, 8, 11]);
         break;
     }
 
-    // EMOJI PARTS
+    // EMOJI BIT
 
-    if (message.content === "reset") {
-      this.servos.each((servo, index) => this.toAngle(servoIndex, 90));
-      // message.channel.send('reset servos to 90 degrees');
+    if (message.content === "test11") {
+      this.arpeggio([11, 11, 11, 11]);
       return;
     }
 
-    if (message.content === "test11") {
-      this.arpege([11, 11, 11, 11]);
+    if (message.content === "reset") {
+      this.servos.each((servo, index) => this.toAngle(servoIndex, 90));
       return;
     }
 
     if (message.content === "min") {
-      this.servos.each((servo, index) => this.toAngle(servoIndex, 0));
+        this.servos.each((servo, index) => this.toAngle(servoIndex, 0));
       return;
     }
 
@@ -192,6 +189,14 @@ class DiscordBotServos {
       1: 1,
       2: 2,
       3: 3,
+      4:4,
+      5:5,
+      6:6,
+      7:7,
+      8:8,
+      9:9,
+      "dix":10,
+      "onze":11,
     };
 
     if (!(char in table)) return;
